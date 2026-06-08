ae9b87d2f4ba402f59372a3751711ec62bcd194b
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-06-08 21:43:09 +0530

    Time: 31 ms (64.11%) | Memory: 95.8 MB (27.79%) - LeetSync

--- Day-06/128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ Day-06/128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -23,23 +23,46 @@ class Solution {
 
         // Better Approach:-
 
-        if(nums.length == 0) return 0;
-        Arrays.sort(nums);
+        // if(nums.length == 0) return 0;
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // int longest = 1;
+        // int count  = 0;
+        // int lastsmall = Integer.MIN_VALUE;
+        // for(int i = 0 ; i<n ; i++){
+        //     if((nums[i]-1)==lastsmall){
+        //         count = count + 1;
+        //         lastsmall=nums[i];
+        //     }
+        //     else if(nums[i]!=lastsmall){
+        //         count=1;
+        //         lastsmall=nums[i];
+        //     }
+        //     longest = Math.max(longest,count);
+        // }
+        // return longest;
+
+        // Optimal Approach:-
+
         int n = nums.length;
-        int longest = 1;
-        int count  = 0;
-        int lastsmall = Integer.MIN_VALUE;
-        for(int i = 0 ; i<n ; i++){
-            if((nums[i]-1)==lastsmall){
-                count = count + 1;
-                lastsmall=nums[i];
+        if (n == 0) return 0;
+        int longest = 1; 
+        Set<Integer> st = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            st.add(nums[i]);
+        }
+        for (int it : st) {
+            if (!st.contains(it - 1)) {
+                int cnt = 1; 
+                int x = it; 
+                while (st.contains(x + 1)) {
+                    x = x + 1; 
+                    cnt = cnt + 1; 
+                }
+                longest = Math.max(longest, cnt);
             }
-            else if(nums[i]!=lastsmall){
-                count=1;
-                lastsmall=nums[i];
-            }
-            longest = Math.max(longest,count);
         }
         return longest;
+
 }
 }
