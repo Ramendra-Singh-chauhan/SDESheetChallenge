cf4b49f6c953059fbc0f0cae4b9e75e9ba6fca6e
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-06-10 12:18:31 +0530

    Time: 1 ms (98.43%) | Memory: 47.6 MB (61.09%) - LeetSync

--- Day-07/73-set-matrix-zeroes/set-matrix-zeroes.java
+++ Day-07/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,35 +1,60 @@
 class Solution {
-    public void markRow(int[][] matrix , int row){
-        for(int j=0;j<matrix[0].length;j++){
-            if(matrix[row][j]!=0){
-                matrix[row][j]=99999;
-            }
-        }
-    }
-    public void markColoumn(int[][] matrix , int coloumn){
-        for(int i=0;i<matrix.length;i++){
-            if(matrix[i][coloumn]!=0){
-                matrix[i][coloumn]=99999;
-            }
-        }
-    }
+    // brute force Approach:-
+    // public void markRow(int[][] matrix , int row){
+    //     for(int j=0;j<matrix[0].length;j++){
+    //         if(matrix[row][j]!=0){
+    //             matrix[row][j]=99999;
+    //         }
+    //     }
+    // }
+    // public void markColoumn(int[][] matrix , int coloumn){
+    //     for(int i=0;i<matrix.length;i++){
+    //         if(matrix[i][coloumn]!=0){
+    //             matrix[i][coloumn]=99999;
+    //         }
+    //     }
+    // }
     
     public void setZeroes(int[][] matrix) {
-        
-     for(int i=0 ; i<matrix.length ;i++){
-        for(int j=0 ; j<matrix[0].length ;j++){
-           if(matrix[i][j]==0){
-            markRow(matrix,i);
-            markColoumn(matrix,j);
-           }
+    // //  brute force Approach:-
+    //  for(int i=0 ; i<matrix.length ;i++){
+    //     for(int j=0 ; j<matrix[0].length ;j++){
+    //        if(matrix[i][j]==0){
+    //         markRow(matrix,i);
+    //         markColoumn(matrix,j);
+    //        }
+    //     }
+    //  }
+    //  for(int i=0 ; i<matrix.length ;i++){
+    //     for(int j=0 ; j<matrix[0].length ;j++){
+    //        if(matrix[i][j] == 99999){
+    //         matrix[i][j]=0;
+    //        }
+    //     }
+    //  }       
+
+
+    // Better apprroach:-
+
+    int m = matrix.length;
+    int n = matrix[0].length;
+    boolean [] row = new boolean[m];
+    boolean [] coloumn = new boolean[n];
+    for(int i = 0 ; i<m ; i++){
+        for(int j = 0 ; j<n ; j++){
+            if(matrix[i][j]==0){
+                row[i]=true;
+                coloumn[j]=true;
+            }
+        }
+    }
+    for(int i = 0 ; i<m ; i++){
+        for(int j = 0 ; j<n ; j++){
+            if(row[i] || coloumn[j]){
+                matrix[i][j]=0;
+            }
         }
      }
-     for(int i=0 ; i<matrix.length ;i++){
-        for(int j=0 ; j<matrix[0].length ;j++){
-           if(matrix[i][j] == 99999){
-            matrix[i][j]=0;
-           }
-        }
-     }       
+    //  return matrix;
    }
 }
