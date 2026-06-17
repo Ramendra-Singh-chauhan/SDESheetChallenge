12ff6ef7e1cffeae3748ab58f7b2a485af0c2538
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-06-17 16:58:25 +0530

    Time: 31 ms (83.20%) | Memory: 59.1 MB (73.73%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -18,21 +18,45 @@ class Solution {
 
 
     // Better Approach:-
-        Set<List<Integer>> set = new HashSet<>();
-        for(int i = 0 ; i<nums.length ; i++){
-            Set<Integer> hashset = new HashSet<>();
-            for(int j = i+1 ; j<nums.length ; j++){
-                int third = -(nums[i]+nums[j]);
-                if(hashset.contains(third)){
-                     List<Integer> temp = Arrays.asList(nums[i], nums[j], third);
-                     Collections.sort(temp);
-                     set.add(temp);
+        // Set<List<Integer>> set = new HashSet<>();
+        // for(int i = 0 ; i<nums.length ; i++){
+        //     Set<Integer> hashset = new HashSet<>();
+        //     for(int j = i+1 ; j<nums.length ; j++){
+        //         int third = -(nums[i]+nums[j]);
+        //         if(hashset.contains(third)){
+        //              List<Integer> temp = Arrays.asList(nums[i], nums[j], third);
+        //              Collections.sort(temp);
+        //              set.add(temp);
+        //         }
+        //         hashset.add(nums[j]);
+        //     }
+        // }
+        // List<List<Integer>> ans = new ArrayList<>(set);
+        // return ans;
+
+        // Optimal Approach:-
+        Arrays.sort(nums);
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        for(int i = 0 ; i<n ; i++){
+            if(nums[i] > 0) break;
+            if(i > 0 && nums[i]==nums[i-1]) continue;
+            int left = i+1 , right = n-1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum==0){
+                ans.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+                left++;
+                right--;
+                // Skip duplicates for left
+                while (left < right && nums[left] == nums[left - 1]) left++;
+                // Skip duplicates for right
+                while (left < right && nums[right] == nums [right + 1]) right--;
                 }
-                hashset.add(nums[j]);
+                else if (sum < 0) left++;
+                else right--;
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(set);
-
-    return ans;
+        return ans;
     }
 }
