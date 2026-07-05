1a7e1e2b2cb4ac7183813ae8de93155dd4ab13d7
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-05 22:25:42 +0530

    Time: 1 ms (99.97%) | Memory: 47 MB (45.79%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,16 +1,23 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        for(int i = 0 ; i<n ; i++){
-            int count = 0;
-            for(int j = 0 ; j<n ; j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count==1) return nums[i];
-        }
-        return -1;
+        //Brute force :- O(N*N) nested loop
+
+        // int n = nums.length;
+        // for(int i = 0 ; i<n ; i++){
+        //     int count = 0;
+        //     for(int j = 0 ; j<n ; j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1) return nums[i];
+        // }
+        // return -1;
+
+
+
+        // better approach:- using hashing
+
         // HashMap<Integer , Integer> map = new HashMap<>();
         // for(int i = 0 ; i<nums.length ; i++){
         //     map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
@@ -21,5 +28,13 @@ class Solution {
         //     }
         // }
         // return -1;
+
+        // optimal solution :- XOR 
+
+        int XOR = 0;
+        for(int i = 0 ; i<nums.length ; i++){
+            XOR = XOR ^ nums[i];
+        }
+        return XOR;
     }
 }
