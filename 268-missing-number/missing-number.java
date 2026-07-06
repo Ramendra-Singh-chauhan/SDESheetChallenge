419e866bb73f8bacb4d17689fbc3efaf74a36ab5
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-06 12:50:51 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (32.32%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -15,17 +15,28 @@ class Solution {
         // return -1;
 
         // better Approach
+
+        // int n = nums.length;
+        // int[] hash = new int[n+1];
+        // Arrays.fill(hash,0);
+        // for(int i = 0 ; i< n ; i++){
+        //     hash[nums[i]]++;
+        // }
+        // for(int i = 0 ; i<hash.length ; i++){
+        //     if(hash[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // optimal Approach
+
+        int sum = 0 ;
         int n = nums.length;
-        int[] hash = new int[n+1];
-        Arrays.fill(hash,0);
-        for(int i = 0 ; i< n ; i++){
-            hash[nums[i]]++;
+        for(int i = 0 ; i<n ; i++){
+            sum+=nums[i];
         }
-        for(int i = 0 ; i<hash.length ; i++){
-            if(hash[i]==0){
-                return i;
-            }
-        }
-        return -1;
+        int totalSum = (n*(n+1))/2;
+        return (totalSum-sum);
     }
 }
