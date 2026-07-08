9799323d60b8d5fbd50ae6250dc20426015f43d0
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-08 14:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (76.96%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
+        // brute force :-
+        // for(int i=0 ; i<nums.length ; i++){
+        //     for(int j=i; j<nums.length ; j++){
+        //         if(nums[i]>nums[j]){ 
+        //             int temp = nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //         }
+        //     }
+        // }
+
+        // better approach
+        int zero=0 , one=0 , two=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0) zero++;
+            else if(nums[i]==1) one++;
+            else two++;
+        }
+        for(int i=0; i<zero; i++) nums[i]=0;
+        for(int i=zero; i<(zero+one); i++) nums[i]=1; 
+        for(int i=zero+one; i<nums.length; i++) nums[i]=2;
+
         // int  low = 0, mid = 0, high = nums.length-1;
         // while(mid<=high){
         // if(nums[mid] == 0){
@@ -19,6 +41,6 @@ class Solution {
         //    high--;
         // }
         // }
-        Arrays.sort(nums);
+        
     }
 }
