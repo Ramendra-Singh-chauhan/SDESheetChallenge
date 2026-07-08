fdedebb2ae6cb5cda972f6ffb68bf1a0e37a4424
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-08 14:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (10.71%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -12,35 +12,37 @@ class Solution {
         // }
 
         // better approach
-        int zero=0 , one=0 , two=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==0) zero++;
-            else if(nums[i]==1) one++;
-            else two++;
-        }
-        for(int i=0; i<zero; i++) nums[i]=0;
-        for(int i=zero; i<(zero+one); i++) nums[i]=1; 
-        for(int i=zero+one; i<nums.length; i++) nums[i]=2;
+        // int zero=0 , one=0 , two=0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i]==0) zero++;
+        //     else if(nums[i]==1) one++;
+        //     else two++;
+        // }
+        // for(int i=0; i<zero; i++) nums[i]=0;
+        // for(int i=zero; i<(zero+one); i++) nums[i]=1; 
+        // for(int i=zero+one; i<nums.length; i++) nums[i]=2;
 
-        // int  low = 0, mid = 0, high = nums.length-1;
-        // while(mid<=high){
-        // if(nums[mid] == 0){
-        //    int temp = nums[low];
-        //    nums[low] = nums[mid];
-        //    nums[mid] = temp;
-        //    mid++;
-        //    low++;
-        // }
-        // else if(nums[mid] == 1){
-        //    mid++;
-        // }
-        // else{
-        //    int temp = nums[mid];
-        //    nums[mid] = nums[high];
-        //    nums[high] = temp;
-        //    high--;
-        // }
-        // }
-        
+
+
+        // optimal approach
+        int low=0 , mid=0 , high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
     }
 }
