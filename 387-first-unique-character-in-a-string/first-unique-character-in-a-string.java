c8fb3833c0e14145cf3a71432d6085859a5cb5c2
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-13 14:24:02 +0530

    Time: 6 ms (84.42%) | Memory: 47.4 MB (6.32%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -14,20 +14,33 @@ class Solution {
         // }
         // return -1;
 
-        // optimal
-        HashMap<Character , Integer> map = new HashMap<>();
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0 ; i<s.length() ; i++){
-            char ch = s.charAt(i);
+        // better
+        // HashMap<Character , Integer> map = new HashMap<>();
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i=0 ; i<s.length() ; i++){
+        //     char ch = s.charAt(i);
 
-            if(!map.containsKey(ch)){
-                q.offer(i);
+        //     if(!map.containsKey(ch)){
+        //         q.offer(i);
+        //     }
+        //     map.put(ch ,map.getOrDefault(ch,0)+1);
+        // }
+        // while(!q.isEmpty() && map.get(s.charAt(q.peek()))>1){
+        //     q.poll();
+        // }
+        // return q.isEmpty() ? -1 : q.peek();
+
+        // optimal
+        int[] count = new int[26];
+
+        for(int i=0 ; i<s.length() ;i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0 ; i<s.length() ;i++){
+            if(count[s.charAt(i)-'a']==1){
+                return i;
             }
-            map.put(ch ,map.getOrDefault(ch,0)+1);
         }
-        while(!q.isEmpty() && map.get(s.charAt(q.peek()))>1){
-            q.poll();
-        }
-        return q.isEmpty() ? -1 : q.peek();
+        return -1;
     }
 }
