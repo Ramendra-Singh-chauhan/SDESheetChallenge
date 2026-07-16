65f9cdfb4ca264e145331e4cc8177e4ee7d4a45e
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-16 15:42:21 +0530

    Time: 7 ms (64.69%) | Memory: 44.4 MB (47.59%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -5,22 +5,23 @@ class Solution {
 
 
         // brute force 
-        String[] brr = new String[arr.length];
-        int left = 0;
-        int right = arr.length-1;
-        while(right>=0){
-            brr[left]=arr[right];
-            left++;
-            right--;
-        }
-        return String.join(" ",brr);
-
-        // for(int i = arr.length-1 ; i>=0 ; i--){
-        //     if(arr[i].length()>0){
-        //         sb.append(arr[i]);
-        //         sb.append(" ");
-        //     }
+        // String[] brr = new String[arr.length];
+        // int left = 0;
+        // int right = arr.length-1;
+        // while(right>=0){
+        //     brr[left]=arr[right];
+        //     left++;
+        //     right--;
         // }
-        // return sb.substring(0, sb.length() - 1).toString();
+        // return String.join(" ",brr);
+
+        // better Approach
+        for(int i = arr.length-1 ; i>=0 ; i--){
+            if(arr[i].length()>0){
+                sb.append(arr[i]);
+                sb.append(" ");
+            }
+        }
+        return sb.substring(0, sb.length() - 1).toString();
     }
 }
