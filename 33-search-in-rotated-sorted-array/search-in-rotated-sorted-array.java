5a8e12c47e259c01b9c2e526ddbce31ad30330fe
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-17 10:20:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (92.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,68 +1,66 @@
 class Solution {
     // brute force
-    // int findK(int[] arr){
-    //     int n=arr.length;
-    //     int k = 0 ;
-    //     for(int i=1 ; i<arr.length ; i++){
-    //         if(arr[i]>=arr[i-1]){
-    //             continue;
-    //         }
-    //         else{
-    //             k=i-1;
-    //         }
-    //     }
-    //     return k;
-    // }
-    // int binarySearch(int[] arr , int low , int high , int target){
-    //     while(low<=high){
-    //         int mid=(low+high)/2;
-    //         if(arr[mid]==target) return mid;
-    //         else if(arr[mid]<target){
-    //             low=mid+1;
-    //         }
-    //         else{
-    //             high=mid-1;
-    //         }
-    //     }
-    //     return -1;
-    // }
-    public int search(int[] nums, int target) {
-        // brute force
-        // int k=findK(nums);
-        // int n = nums.length;
-        // int low=0;
-        // int high=n-1;
-        // int mid= (low+high)/2;
-        // int ans=binarySearch(nums , 0 , mid , target);
-        // if(ans!=-1) return ans;
-        // return binarySearch(nums , mid+1 , n-1 , target);
-
-        // optimal 
-        int n=nums.length;
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target) return mid;
-
-            // left side is sorted
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<=nums[mid]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
+    int findK(int[] arr){
+        int k=0;
+        for(int i=0 ; i<arr.length-1 ; i++){
+            if(arr[i+1]>arr[i]){
+                continue;
             }
             else{
-                if(target>nums[mid] && target<=nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+                k=i;
+            }
+        }
+        return k;
+    }
+    int BinarySearch(int[] arr , int low , int high, int target){
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(arr[mid]==target) return mid;
+
+            else if(arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return -1;
+    }
+    public int search(int[] nums, int target) {
+        // brute force
+        int n = nums.length;
+        int k = findK(nums);
+        int ans = BinarySearch(nums , 0 , k , target);
+        if(ans!=-1) return ans;
+        return BinarySearch(nums,k+1,n-1,target);
+        // // optimal 
+        // int n=nums.length;
+        // int low=0;
+        // int high=n-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]==target) return mid;
+
+        //     // left side is sorted
+        //     if(nums[low]<=nums[mid]){
+        //         if(target>=nums[low] && target<=nums[mid]){
+        //             high=mid-1;
+        //         }
+        //         else{
+        //             low=mid+1;
+        //         }
+        //     }
+        //     // right side is sorted
+        //     else{
+        //         if(target>nums[mid] && target<=nums[high]){
+        //             low=mid+1;
+        //         }
+        //         else{
+        //             high=mid-1;
+        //         }
+        //     }
+        // }
+        // return -1;
     }
 }
