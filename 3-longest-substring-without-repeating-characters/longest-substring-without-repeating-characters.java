c70de1d7b9c2fabd9e8654c47eaf5023f0e18d44
Ramendra Singh Chauhan <ramendrasinghchouhan870@gmail.com>
2026-07-23 12:55:36 +0530

    Time: 3 ms (89.14%) | Memory: 44.9 MB (91.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,36 +1,38 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         // Brute Force Approach:-
-        int maxlength=0;
-        for(int i=0 ; i<s.length() ; i++){
-            int[] hashArray = new int[256];
-            Arrays.fill(hashArray,0);
-            for(int j=i ; j<s.length() ; j++){
-                if(hashArray[s.charAt(j)]==1) break;
-                hashArray[s.charAt(j)]=1;
-                int length = j-i+1;
-                maxlength=Math.max(length,maxlength);
-            }
-        }
-        return maxlength;
+        // int maxlength=0;
+        // for(int i=0 ; i<s.length() ; i++){
+        //     int[] hashArray = new int[256];
+        //     Arrays.fill(hashArray,0);
+        //     for(int j=i ; j<s.length() ; j++){
+        //         if(hashArray[s.charAt(j)]==1) break;
+        //         hashArray[s.charAt(j)]=1;
+        //         int length = j-i+1;
+        //         maxlength=Math.max(length,maxlength);
+        //     }
+        // }
+        // return maxlength;
 
 
         // Optimal Approach:-
 
-        // int n = s.length();
+        int n = s.length();
         // int HashLen = 256;
-        // int[] hash = new int[HashLen];
-        // Arrays.fill(hash, -1);
-        // int l = 0, r = 0, maxLen = 0;
-        // while (r < n) {
-        //     if (hash[s.charAt(r)] >= l) {
-        //         l = Math.max(hash[s.charAt(r)] + 1, l);
-        //     }
-        //     int len = r - l + 1;
-        //     maxLen = Math.max(len, maxLen);
-        //     hash[s.charAt(r)] = r;
-        //     r++;
-        // }
-        // return maxLen;
+        int[] hash = new int[256];
+        Arrays.fill(hash, -1);
+        int l = 0, r = 0, maxLen = 0;
+        while (r < n) {
+            if(hash[s.charAt(r)]!=-1){
+                if (hash[s.charAt(r)] >= l){
+                    l = Math.max(hash[s.charAt(r)] + 1, l);
+                }
+            }
+            int len = r - l + 1;
+            maxLen = Math.max(len, maxLen);
+            hash[s.charAt(r)] = r;
+            r++;
+        }
+        return maxLen;
     }
 }
